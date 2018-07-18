5e6b4f43ac2b02956f5f92ac6f4a0bb3b7c7747d
Ed Schmerling <ednerd@gmail.com>
2018-07-17 17:32:38 -0700

    Updates for 0.7 (dropping 0.6) + slight refactor

--- README.md
+++ README.md
@@ -1,7 +1,4 @@
 # DifferentialDynamicsModels
 
 [![Build Status](https://travis-ci.org/schmrlng/DifferentialDynamicsModels.jl.svg?branch=master)](https://travis-ci.org/schmrlng/DifferentialDynamicsModels.jl)
-
-[![Coverage Status](https://coveralls.io/repos/schmrlng/DifferentialDynamicsModels.jl/badge.svg?branch=master&service=github)](https://coveralls.io/github/schmrlng/DifferentialDynamicsModels.jl?branch=master)
-
 [![codecov.io](http://codecov.io/github/schmrlng/DifferentialDynamicsModels.jl/coverage.svg?branch=master)](http://codecov.io/github/schmrlng/DifferentialDynamicsModels.jl?branch=master)
--- src/DifferentialDynamicsModels.jl
+++ src/DifferentialDynamicsModels.jl
@@ -3,15 +3,37 @@ __precompile__()
 module DifferentialDynamicsModels
 
 using StaticArrays
+using LinearAlgebra
+macro maintain_type(expr)    # ensures that subtypes of StaticArrays.FieldVector maintain their type upon manipulation
+    full_name = expr.args[2].args[1]                                     # MyType{T} or MyType
+    short_name = full_name isa Symbol ? full_name : full_name.args[1]    # MyType
+    @assert expr.args[2].args[2].args[1] == :FieldVector "@maintain_type is defined only for subtypes of FieldVector"
+    N = expr.args[2].args[2].args[2]                                     # N in FieldVector{N,T}
+    T = expr.args[2].args[2].args[3]                                     # T in FieldVector{N,T}
+    st_method = if short_name == full_name
+        :(StaticArrays.similar_type(::Type{$short_name}, ::Type{$T}, s::Size{($N,)}) = $short_name)
+    else
+        :(StaticArrays.similar_type(::Type{<:$short_name}, ::Type{$T}, s::Size{($N,)}) where {$T} = $full_name)
+    end
+    quote
+        $(esc(expr))
+        $(esc(st_method))
+    end
+end
+
+export @maintain_type
 export AbstractState, State, AbstractControl, Control, DifferentialDynamics, CostFunctional
 export Time, TimePlusQuadraticControl
-export SteeringBVP, SteeringParams, SteeringCache, EmptySteeringParams, EmptySteeringCache
-import Base.issymmetric
-export Path, ControlInterval, ControlSequence
-export StepControl, BVPControl, TimestampedState, InterpolationInterval
-export SingleIntegratorDynamics
+export StateSequence, ControlInterval, ControlSequence
+export StepControl, RampControl, BVPControl
+export SteeringBVP, SteeringConstraints, SteeringCache, EmptySteeringConstraints, EmptySteeringCache
+export SingleIntegratorDynamics, BoundedControlNorm
 export state_dim, control_dim, duration, propagate, instantaneous_control, waypoints
+import Base: zero
+import LinearAlgebra: issymmetric
+export issymmetric
 
+# States, Controls, Dynamics, and Cost Functionals
 abstract type AbstractState end
 const State = Union{AbstractState, AbstractVector}
 abstract type AbstractControl end
@@ -23,32 +45,17 @@ struct TimePlusQuadraticControl{Du,T,DuDu} <: CostFunctional
     R::SMatrix{Du,Du,T,DuDu}
 end
 
-abstract type SteeringParams end
-abstract type SteeringCache end
-struct EmptySteeringParams <: SteeringParams end
-struct EmptySteeringCache <: SteeringCache end
-struct SteeringBVP{D<:DifferentialDynamics,C<:CostFunctional,SP<:SteeringParams,SC<:SteeringCache}
-    dynamics::D
-    cost::C
-    params::SP
-    cache::SC
-end
-function SteeringBVP(dynamics::DifferentialDynamics, cost::CostFunctional)
-    SteeringBVP(dynamics, cost, EmptySteeringParams(), EmptySteeringCache())
-end
-function SteeringBVP(dynamics::DifferentialDynamics, cost::CostFunctional, params::SteeringParams)
-    SteeringBVP(dynamics, cost, params, EmptySteeringCache())
-end
-issymmetric(bvp::SteeringBVP) = false
-(bvp::SteeringBVP)(x0::State, xf::State, cost_bound::AbstractFloat) = bvp(x0, xf)    # general fallback
-
-const Path{S} = AbstractVector{S} where {S<:State}
+# State/Control Sequences
+const StateSequence{S} = AbstractVector{S} where {S<:State}
 abstract type ControlInterval end
 const ControlSequence{CI} = AbstractVector{CI} where {CI<:ControlInterval}
 duration(cs::ControlSequence) = sum(duration(c) for c in cs)
+zero(::CI) where {CI<:ControlInterval} = zero(CI)
 
-propagate(f::DifferentialDynamics, x::State, cs::ControlSequence) = foldl((x,c) -> propagate(f,x,c), x, cs)
-function propagate(f::DifferentialDynamics, x::State, cs::ControlSequence, s::AbstractFloat)
+## Propagation (state as a function of time)
+propagate(f::DifferentialDynamics, x::State, cs::ControlSequence) = foldl((x,c) -> propagate(f,x,c), cs, init=x)
+propagate(f::DifferentialDynamics, x::State, c::ControlInterval, ss) = map(s -> propagate(f, x, c, s), ss)
+function propagate(f::DifferentialDynamics, x::State, cs::ControlSequence, s::Number)
     s <= 0 && return x
     t = zero(s)
     for c in cs
@@ -61,16 +68,13 @@ function propagate(f::DifferentialDynamics, x::State, cs::ControlSequence, s::Ab
     end
     x
 end
-function propagate(f::DifferentialDynamics, x::State, c::ControlInterval, ss::AbstractVector)
-    [propagate(f, x, c, s) for s in ss]
-end
-function propagate(f::DifferentialDynamics, x::State, cs::ControlSequence, ss::AbstractVector)
-    issorted(ss) || error("Times should be sorted as input to propagate.")
+function propagate(f::DifferentialDynamics, x::State, cs::ControlSequence, ss)
     tf = duration(cs)
-    ss = clamp.(ss, zero(tf), tf)
     path = typeof(x)[]
-    t, i = zero(tf), 1
+    prev_s, t, i = zero(tf), zero(tf), 1
     for s in ss
+        s = clamp(s, zero(tf), tf)
+        @assert s >= prev_s "Input times must be sorted."
         @inbounds c = cs[i]
         while s >= t + duration(c) && i < length(cs)    # second clause necessary because of numerical error
             x = propagate(f, x, c)
@@ -79,69 +83,54 @@ function propagate(f::DifferentialDynamics, x::State, cs::ControlSequence, ss::A
             @inbounds c = cs[i]
         end
         push!(path, propagate(f, x, c, s-t))
+        prev_s = s
     end
     path
 end
 
-function instantaneous_control(f::DifferentialDynamics, x::State, cs::ControlSequence, s::AbstractFloat)
-    s <= 0 && return instantaneous_control(f, x, cs[1], zero(s))
-    x_prev = x
+## Instantaneous Controls (control as a function of time)
+instantaneous_control(c::ControlInterval, ss) = map(s -> instantaneous_control(c, s), ss)
+function instantaneous_control(cs::ControlSequence, s::Number)
+    s <= 0 && return instantaneous_control(cs[1], zero(s))
     t = zero(s)
     for c in cs
         if s >= t + duration(c)
-            x_prev = x
-            x = propagate(f, x, c)
             t += duration(c)
         else
-            return instantaneous_control(f, x, c, s-t)
+            return instantaneous_control(c, s-t)
         end
     end
-    instantaneous_control(f, x_prev, cs[end], duration(cs[end]))
+    instantaneous_control(c[end], duration(c[end]))
 end
-function instantaneous_control(f::DifferentialDynamics, x::State, c::ControlInterval, ss::AbstractVector)
-    [instantaneous_control(f, x, c, s) for s in ss]
-end
-function instantaneous_control(f::DifferentialDynamics, x::State, cs::ControlSequence, ss::AbstractVector)
-    issorted(ss) || error("Times should be sorted as input to instantaneous_control.")
+function instantaneous_control(cs::ControlSequence, ss)
     tf = duration(cs)
-    ss = clamp.(ss, zero(tf), tf)
-    ics = typeof(instantaneous_control(f, x, cs[1], zero(tf)))[]
-    t, i = zero(tf), 1
+    ics = typeof(instantaneous_control(cs[1], zero(tf)))[]
+    prev_s, t, i = zero(tf), zero(tf), 1
     for s in ss
+        s = clamp(s, zero(tf), tf)
+        @assert s >= prev_s "Input times must be sorted."
         @inbounds c = cs[i]
         while s >= t + duration(c) && i < length(cs)    # second clause necessary because of numerical error
-            x = propagate(f, x, c)
             t += duration(c)
             i += 1
             @inbounds c = cs[i]
         end
-        push!(ics, instantaneous_control(f, x, c, s-t))
+        push!(ics, instantaneous_control(c, s-t))
+        prev_s = s
     end
     ics
 end
 
+## Waypoints (convenience methods for state propagation)
 function waypoints(f::DifferentialDynamics, x::State, c::Union{ControlInterval, ControlSequence}, dt::AbstractFloat)
     propagate(f, x, c, 0:dt:oftype(dt, duration(c)))
 end
 function waypoints(f::DifferentialDynamics, x::State, c::Union{ControlInterval, ControlSequence}, N::Int)
-    propagate(f, x, c, linspace(0, duration(c), N))
+    propagate(f, x, c, range(0, stop=duration(c), length=N))
 end
 
-### Control Intervals
-## Step Control
-struct StepControl{N,T,S<:StaticVector{N,T}} <: ControlInterval
-    t::T
-    u::S
-end
-(::Type{SC})(t::T, u::S) where {N,T,S<:StaticVector{N,T},SC<:StepControl} = StepControl{N,T,S}(t, u)
-duration(c::StepControl) = c.t
-Base.zero(x::StepControl{N,T,S}) where {N,T,S} = StepControl(T(0), zeros(S))
-Base.zero(x::Type{StepControl{N,T,S}}) where {N,T,S} = StepControl(T(0), zeros(S))
-
-function propagate(f::DifferentialDynamics, x::State, c::StepControl, s::AbstractFloat)
-    s <= 0 ? x : s >= duration(c) ? propagate(f, x, c) : propagate(f, x, StepControl(s,c.u))
-end
-function propagate_heun{T}(fn::Function, y0::AbstractArray{T}, Tf::T, Ti::T = T(0), N::Int = 10)
+# Control Intervals
+function propagate_heun(fn, y0, Tf, Ti=zero(Tf), N=10)
     dt = (Tf - Ti)/N
     y = y0
     t = Ti
@@ -153,7 +142,7 @@ function propagate_heun{T}(fn::Function, y0::AbstractArray{T}, Tf::T, Ti::T = T(
     end
     y
 end
-function propagate_rk4{T}(fn::Function, y0::AbstractArray{T}, Tf::T, Ti::T = T(0), N::Int = 10)
+function propagate_rk4(fn, y0, Tf, Ti=zero(Tf), N=10)
     dt = (Tf - Ti)/N
     y = y0
     t = Ti
@@ -167,72 +156,102 @@ function propagate_rk4{T}(fn::Function, y0::AbstractArray{T}, Tf::T, Ti::T = T(0
     end
     y
 end
-function propagate_ODE(f::DifferentialDynamics, x::State, c::StepControl, N::Int = 10)
-    typeof(x)(c.t > 0 ? propagate_rk4((y,t) -> f(y, c.u), x, c.t, zero(c.t), N) :
-                        propagate_rk4((y,t) -> -f(y, c.u), x, -c.t, zero(c.t), N))
+function propagate_ODE(f::DifferentialDynamics, x::State, c::ControlInterval, N=10)
+    c.t > 0 ? propagate_rk4((y,t) -> f(y, instantaneous_control(c, t)), x, c.t, zero(c.t), N) :
+              propagate_rk4((y,t) -> -f(y, instantaneous_control(c, -t)), x, -c.t, zero(c.t), N)
 end
-propagate(f::DifferentialDynamics, x::State, c::StepControl) = propagate_ODE(f, x, c)    # general fallback
-instantaneous_control(f::DifferentialDynamics, x::State, c::StepControl, s::AbstractFloat) = c.u
+propagate(f::DifferentialDynamics, x::State, c::ControlInterval) = propagate_ODE(f, x, c)    # general fallback
+
+## Step Control
+struct StepControl{N,T,S<:StaticVector{N,T}} <: ControlInterval
+    t::T
+    u::S
+    (::Type{SC})(t::T, u::S) where {N,T,S<:StaticVector{N,T},SC<:StepControl} = new{N,T,S}(t, u)
+end
+const ZeroOrderHoldControl{N,T,S} = StepControl{N,T,S}
+duration(c::StepControl) = c.t
+zero(x::Type{StepControl{N,T,S}}) where {N,T,S} = StepControl(T(0), zeros(S))
+function propagate(f::DifferentialDynamics, x::State, c::StepControl, s::Number)
+    s <= 0           ? x :
+    s >= duration(c) ? propagate(f, x, c) :
+                       propagate(f, x, StepControl(s, c.u))
+end
+instantaneous_control(c::StepControl, s::Number) = c.u
+
+## Ramp Control
+struct RampControl{N,T,S<:StaticVector{N,T}} <: ControlInterval
+    t::T
+    u0::S
+    uf::S
+    (::Type{RC})(t::T, u0::S, uf::S) where {N,T,S<:StaticVector{N,T},RC<:RampControl} = new{N,T,S}(t, u0, uf)
+end
+const FirstOrderHoldControl{N,T,S} = RampControl{N,T,S}
+duration(c::RampControl) = c.t
+zero(x::Type{RampControl{N,T,S}}) where {N,T,S} = RampControl(T(0), zeros(S), zeros(S))
+function propagate(f::DifferentialDynamics, x::State, c::RampControl, s::Number)
+    s <= 0           ? x :
+    s >= duration(c) ? propagate(f, x, c) :
+                       propagate(f, x, RampControl(s, c.u0, instantaneous_control(c, s)))
+end
+instantaneous_control(c::RampControl, s::Number) = c.u0 + (s/c.t)*(c.uf - c.u0)
 
 ## BVP Control
 struct BVPControl{T,S<:State,Fx<:Function,Fu<:Function} <: ControlInterval
     t::T
-    y::S
+    x0::S
+    xf::S
     x::Fx
     u::Fu
 end
 duration(c::BVPControl) = c.t
+propagate(f::DifferentialDynamics, x::State, c::BVPControl) = (x - c.x0) + c.xf
+propagate(f::DifferentialDynamics, x::State, c::BVPControl, s::Number) = (x - c.x0) + c.x(c.x0, c.xf, c.t, s)
+instantaneous_control(f::DifferentialDynamics, x::State, c::BVPControl, s::Number) = c.u(c.x0, c.xf, c.t, s)
 
-propagate(f::DifferentialDynamics, x::State, c::BVPControl) = c.x
-propagate(f::DifferentialDynamics, x::State, c::BVPControl, s::AbstractFloat) = c.x(x, c.y, c.t, s)
-instantaneous_control(f::DifferentialDynamics, x::State, c::BVPControl, s::AbstractFloat) = c.u(x, c.y, c.t, s)
+# Steering Two-Point Boundary Value Problems (BVPs)
+abstract type SteeringConstraints end
+abstract type SteeringCache end
+struct EmptySteeringConstraints <: SteeringConstraints end
+struct EmptySteeringCache <: SteeringCache end
+struct SteeringBVP{D<:DifferentialDynamics,C<:CostFunctional,SC<:SteeringConstraints,SD<:SteeringCache}
+    dynamics::D
+    cost::C
+    constraints::SC
+    cache::SD
+end
+function SteeringBVP(dynamics::DifferentialDynamics, cost::CostFunctional;
+                     constraints::SteeringConstraints=EmptySteeringConstraints(),
+                     cache::SteeringCache=EmptySteeringCache())
+    SteeringBVP(dynamics, cost, constraints, cache)
+end
+issymmetric(bvp::SteeringBVP) = false                                         # general fallback
+(bvp::SteeringBVP)(x0::State, xf::State, cost_bound::Number) = bvp(x0, xf)    # general fallback
 
-## State Interpolation Interval
-struct TimestampedState{T,S<:State}
-    t::T
-    x::S
-end
-struct InterpolationInterval{A<:AbstractVector{TS} where {TS<:TimestampedState}}
-    txs::A
-end
-duration(c::InterpolationInterval) = c.txs[end].t
-
-propagate(f::DifferentialDynamics, x::State, c::InterpolationInterval) = c.txs[end].x
-function propagate(f::DifferentialDynamics, x::State, c::InterpolationInterval, s::AbstractFloat)
-    s <= 0 && return x
-    i = findfirst(tx -> (tx.t >= s), c.txs)
-    if i == 0
-        return c.txs[end].x
-    elseif i == 1
-        @inbounds α = s/c.txs[1].t
-        @inbounds return (1 - α)*x + α*c.txs[1].x
-    else
-        @inbounds α = (s - c.txs[i-1].t)/(c.txs[i].t - c.txs[i-1].t)
-        @inbounds return (1 - α)*c.txs[i-1].x + α*c.txs[i].x
-    end
-end
-function instantaneous_control(f::DifferentialDynamics, x::State, c::InterpolationInterval, s::AbstractFloat)
-    error("instantaneous_control makes no sense for InterpolationInterval.")
-    # Maybe InterpolationInterval should use TimestampedStateAndControl? This case should be covered by BVPControl.
-end
-
-### Single Integrator
+# Single Integrator
 struct SingleIntegratorDynamics{N} <: DifferentialDynamics end
+struct BoundedControlNorm{P,T} <: SteeringConstraints
+    b::T
+end
+BoundedControlNorm(b::T=1.0) where {T} = BoundedControlNorm{2,T}(b)
+BoundedControlNorm{P}(b::T) where {P,T} = BoundedControlNorm{P,T}(b)
 
 state_dim(::SingleIntegratorDynamics{N}) where {N} = N
 control_dim(::SingleIntegratorDynamics{N}) where {N} = N
 
-function propagate(f::SingleIntegratorDynamics{N}, x::StaticVector{N,T}, c::StepControl{N,T}) where {N,T}
-    x + c.t*typeof(x)(c.u)
+function propagate(f::SingleIntegratorDynamics{N}, x::StaticVector{N}, c::StepControl{N}) where {N}
+    x + c.t*c.u
+end
+function propagate(f::SingleIntegratorDynamics{N}, x::StaticVector{N}, c::RampControl{N}) where {N}
+    x + c.t*(c.u0 + c.uf)/2
 end
 (::SingleIntegratorDynamics{N})(x::StaticVector{N}, u::StaticVector{N}) where {N} = u
 
-issymmetric(bvp::SteeringBVP{D,C,EmptySteeringParams}) where {D<:SingleIntegratorDynamics,C} = true
-function (bvp::SteeringBVP{SingleIntegratorDynamics{N},Time})(x0::StaticVector{N,T},
-                                                              xf::StaticVector{N,T}) where {N,T}
-    c = norm(xf - x0)
-    ctrl = StepControl(c, inv(c)*(xf - x0))
-    return c, ctrl
+issymmetric(bvp::SteeringBVP{<:SingleIntegratorDynamics,<:CostFunctional,<:BoundedControlNorm}) = true
+function (bvp::SteeringBVP{SingleIntegratorDynamics{N},Time,<:BoundedControlNorm{2}})(x0::StaticVector{N},
+                                                                                      xf::StaticVector{N}) where {N}
+    c = norm(xf - x0)/bvp.constraints.b
+    ctrl = StepControl(c, SVector((xf - x0)/c))    # convert to SVector otherwise control will maintain State type
+    return (cost=c, controls=ctrl)
 end
 
 end # module
